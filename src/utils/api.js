df4e1bb15e27e7191810b8b034f2747e7cf9fd70
rohtash singh <rrohtashssingh@gmail.com>
2024-01-31 12:19:11 +0530

    this is the youtube clone and now it is done

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "342ec5a891msh60b2f2dcf0c35fep1f26dajsnbe91f7d4c4b4",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
